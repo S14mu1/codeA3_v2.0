1e9746a4553020d909a1dd86d8a340c84ce3566a
Z0h4ib <chzohaibasghar@gmail.com>
2025-11-25 14:37:45 +0100

    optimization

--- src/main/scala/Accelerator.scala
+++ src/main/scala/Accelerator.scala
@@ -13,37 +13,45 @@ class Accelerator extends Module {
 
   })
 
-  // State machine states with descriptive names
-  val sIdle :: sFetch :: sStore :: sComplete :: sNorth :: sSouth :: sWest :: sEast :: Nil = Enum(8)
+  // Optimized state machine with fewer states
+  val sIdle :: sRead :: sWrite :: sDone :: sCheckNeighbor :: Nil = Enum(5)
   val currentState = RegInit(sIdle)
   
-  // Position tracking registers
+  // Position and data registers
   val posX = RegInit(0.U(5.W))
   val posY = RegInit(0.U(5.W))
   val outputData = RegInit(0.U(32.W))
   val memAddr = RegInit(0.U(16.W))
+  
+  // Neighbor check counter (0=up, 1=down, 2=left, 3=right)
+  val neighborIdx = RegInit(0.U(2.W))
 
-  // Constants for readability
+  // Constants
   val gridSize = 20.U
   val maxCoord = 19.U
   val outputOffset = 400.U
   val whitePixel = 255.U
   val blackPixel = 0.U
 
-  // Helper functions for address calculation
+  // Address calculation helpers
   def calcInputAddr(xPos: UInt, yPos: UInt): UInt = xPos + gridSize * yPos
   def calcOutputAddr(xPos: UInt, yPos: UInt): UInt = calcInputAddr(xPos, yPos) + outputOffset
   
-  // Default output assignments
+  // Default outputs
   io.writeEnable := false.B
   io.dataWrite := outputData
   io.address := memAddr
   io.done := false.B
 
-  // Check if position is at boundary
+  // Boundary and pixel checks
   val atBorder = (posX === 0.U) || (posX === maxCoord) || (posY === 0.U) || (posY === maxCoord)
-  val pixelIsBlack = io.dataRead === blackPixel
-  val pixelIsWhite = io.dataRead === whitePixel
+  val isBlack = io.dataRead === blackPixel
+  val isWhite = io.dataRead === whitePixel
+  
+  // Next position calculation
+  val nextY = Mux(posY < maxCoord, posY + 1.U, 0.U)
+  val nextX = Mux(posY < maxCoord, posX, posX + 1.U)
+  val processingComplete = (posX >= gridSize) || (posY >= gridSize)
 
   switch(currentState) {
     is(sIdle) {
@@ -51,83 +59,66 @@ class Accelerator extends Module {
         posX := 0.U
         posY := 0.U
         memAddr := 0.U
-        currentState := sFetch
+        currentState := sRead
       }
     }
     
-    is(sFetch) {
-      when(atBorder || pixelIsBlack) {
+    is(sRead) {
+      // Optimization: decide immediately based on current position/data
+      when(atBorder || isBlack) {
+        // Can write black immediately
         outputData := blackPixel
         memAddr := calcOutputAddr(posX, posY)
-        currentState := sStore
+        currentState := sWrite
       }.otherwise {
-        memAddr := calcInputAddr(posX, posY - 1.U)
-        currentState := sNorth
+        // Need to check neighbors, start with first neighbor
+        neighborIdx := 0.U
+        memAddr := calcInputAddr(posX, posY - 1.U) // Check up first
+        currentState := sCheckNeighbor
       }
     }
     
-    is(sStore) {
+    is(sCheckNeighbor) {
+      when(isBlack) {
+        // Found a black neighbor, write black and move on
+        outputData := blackPixel
+        memAddr := calcOutputAddr(posX, posY)
+        currentState := sWrite
+      }.elsewhen(neighborIdx === 3.U) {
+        // Checked all 4 neighbors, all white - write white
+        outputData := whitePixel
+        memAddr := calcOutputAddr(posX, posY)
+        currentState := sWrite
+      }.otherwise {
+        // Check next neighbor
+        neighborIdx := neighborIdx + 1.U
+        memAddr := MuxLookup(neighborIdx, 0.U)(Seq(
+          0.U -> calcInputAddr(posX, posY + 1.U),     // down
+          1.U -> calcInputAddr(posX - 1.U, posY),     // left
+          2.U -> calcInputAddr(posX + 1.U, posY)      // right
+        ))
+        currentState := sCheckNeighbor
+      }
+    }
+    
+    is(sWrite) {
       io.writeEnable := true.B
       
-      // Update position for next iteration
-      when(posY < maxCoord) {
-        posY := posY + 1.U
-      }.otherwise {
-        posX := posX + 1.U
-        posY := 0.U
-      }
+      // Pre-calculate next position and address
+      posY := nextY
+      posX := nextX
+      memAddr := calcInputAddr(nextX, nextY)
       
-      memAddr := calcInputAddr(posX, posY)
-      
-      when(posX < gridSize && posY < gridSize) {
-        currentState := sFetch
+      when(nextX < gridSize && nextY < gridSize) {
+        currentState := sRead
       }.otherwise {
-        currentState := sComplete
+        currentState := sDone
       }
     }
     
-    is(sComplete) {
+    is(sDone) {
       io.done := true.B
-      currentState := sComplete
-    }
-    
-    is(sNorth) {
-      when(pixelIsWhite) {
-        memAddr := calcInputAddr(posX, posY + 1.U)
-        currentState := sSouth
-      }.otherwise {
-        outputData := blackPixel
-        memAddr := calcOutputAddr(posX, posY)
-        currentState := sStore
-      }
-    }
-    
-    is(sSouth) {
-      when(pixelIsWhite) {
-        memAddr := calcInputAddr(posX - 1.U, posY)
-        currentState := sWest
-      }.otherwise {
-        outputData := blackPixel
-        memAddr := calcOutputAddr(posX, posY)
-        currentState := sStore
-      }
-    }
-    
-    is(sWest) {
-      when(pixelIsWhite) {
-        memAddr := calcInputAddr(posX + 1.U, posY)
-        currentState := sEast
-      }.otherwise {
-        outputData := blackPixel
-        memAddr := calcOutputAddr(posX, posY)
-        currentState := sStore
-      }
-    }
-    
-    is(sEast) {
-      memAddr := calcOutputAddr(posX, posY)
-      outputData := Mux(pixelIsWhite, whitePixel, blackPixel)
-      currentState := sStore
+      currentState := sDone
     }
   }
 }
